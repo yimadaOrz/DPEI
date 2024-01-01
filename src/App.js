a773d746fabbcbee6de56857323a03b4569dcc2d
yimadaO_o <yimadaO_o@noreply.gitcode.com>
2024-01-01 14:19:09 -0500

    0101

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
 import React, {useEffect} from "react";
-import LiveChat from './LiveChat/LiveChat';
-import Header from './Header/Header';
-import HomePage from './HomePgae/HomePage';
-import Carousels from './HomePgae/Carousels/Carousels';
-import FlatMarkers from './Gallery/FlatMarkers/FlatMarkers';
+import loadable from '@loadable/component';
+
+
+// import HomePage from './HomePgae/HomePage';
 import AboutUs from './CompanyOverview/AboutUs/AboutUs';
 import ProductAndService from './CompanyOverview/ProductAndService/ProductAndService';
 import HowToOrder from './CompanyOverview/HowToOrder/HowToOrder';
@@ -14,25 +13,25 @@ import Privacy from './CompanyOverview/Privacy/Privacy';
 import ContactUs from './ContactUs/ContactUs';
 import {Provider} from 'react-redux';
 import store from './redux/store/store';
-import SingleMonument from '../src/Gallery/SingleMonument/SingleMonument';
-import SingleMonument_en from '../src/Gallery/SingleMonument_en/SingleMonument_en';
-import ShapedMonument from './Gallery/ShapedMonument/ShapedMonument';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
-import DoubleMonument from './Gallery/DoubleMonument/DoubleMonument';
-import DoubleMonument_en from './Gallery/DoubleMonument_en/DoubleMonument_en';
+const SingleMonument = loadable(() => import('../src/Gallery/SingleMonument/SingleMonument'));
+const SingleMonument_en = loadable(() => import('../src/Gallery/SingleMonument_en/SingleMonument_en'));
+const ShapedMonument = loadable(() => import('./Gallery/ShapedMonument/ShapedMonument'));
 
-import SlantMarkers from './Gallery/SlantMarkers/SlantMarkers';
-import SlantMarkers_en from './Gallery/SlantMarkers_en/SlantMarkers_en';
-import Urn from './Gallery/Urn/Urn';
-import MemorialArches from './Gallery/MemorialArches/MemorialArches';
-import FlatMarkers_en from './Gallery/FlatMarkers_en/FlatMarkers_en';
+const DoubleMonument = loadable(() => import('./Gallery/DoubleMonument/DoubleMonument'));
+const DoubleMonument_en = loadable(() => import('./Gallery/DoubleMonument_en/DoubleMonument_en'));
 
-import FamilyMonument from './Gallery/FamilyMonument/FamilyMonument';
+const SlantMarkers = loadable(() => import('./Gallery/SlantMarkers/SlantMarkers'));
+const SlantMarkers_en = loadable(() => import('./Gallery/SlantMarkers_en/SlantMarkers_en'));
+const Urn = loadable(() => import('./Gallery/Urn/Urn'));
+const MemorialArches =  loadable(() => import('./Gallery/MemorialArches/MemorialArches'));
+const FlatMarkers_en = loadable(() => import('./Gallery/FlatMarkers_en/FlatMarkers_en'));
 
+const FamilyMonument = loadable(() => import('./Gallery/FamilyMonument/FamilyMonument'));
 
-import i18n from './text/i18n';
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+const HomePage = loadable(() => import('./HomePgae/HomePage'));
+const FlatMarkers = loadable(() => import('./Gallery/FlatMarkers/FlatMarkers'));
 
 
 function App() {
@@ -52,56 +51,56 @@ function App() {
             document.head.removeChild(icon);
         };
     }, []);
-  return (
-    <div >
-      <Provider store = {store}>
-      <Router>
-          <Routes>
-            <Route path="/" element={<HomePage/>} />
+    return (
+        <div>
+            <Provider store={store}>
+                <Router>
+                    <Routes>
+                        <Route path="/" element={<HomePage/>}/>
 
-            <Route path="/FlatMarkers" element={<FlatMarkers/>} />
+                        <Route path="/FlatMarkers" element={<FlatMarkers/>}/>
 
-              <Route path="/Urn" element={<Urn/>} />
+                        <Route path="/Urn" element={<Urn/>}/>
 
-              <Route path="/MemorialArches" element={<MemorialArches/>} />
+                        <Route path="/MemorialArches" element={<MemorialArches/>}/>
 
-              <Route path="/FlatMarkers_en" element={<FlatMarkers_en/>} />
-              <Route path="/FamilyMonument" element={<FamilyMonument/>} />
+                        <Route path="/FlatMarkers_en" element={<FlatMarkers_en/>}/>
+                        <Route path="/FamilyMonument" element={<FamilyMonument/>}/>
 
-              <Route path="/SingleMonument" element={<SingleMonument/>} />
+                        <Route path="/SingleMonument" element={<SingleMonument/>}/>
 
-              <Route path="/SingleMonument_en" element={<SingleMonument_en/>} />
+                        <Route path="/SingleMonument_en" element={<SingleMonument_en/>}/>
 
-              <Route path="/SlantMarkers" element={<SlantMarkers/>} />
+                        <Route path="/SlantMarkers" element={<SlantMarkers/>}/>
 
-              <Route path="/SlantMarkers_en" element={<SlantMarkers_en/>} />
+                        <Route path="/SlantMarkers_en" element={<SlantMarkers_en/>}/>
 
-              <Route path="/DoubleMonument" element={<DoubleMonument/>} />
+                        <Route path="/DoubleMonument" element={<DoubleMonument/>}/>
 
-              <Route path="/ShapedMonument" element={<ShapedMonument/>} />
+                        <Route path="/ShapedMonument" element={<ShapedMonument/>}/>
 
-              <Route path="/DoubleMonument_en" element={<DoubleMonument_en/>} />
+                        <Route path="/DoubleMonument_en" element={<DoubleMonument_en/>}/>
 
-            <Route path="/About_us" element={<AboutUs/>} /> 
+                        <Route path="/About_us" element={<AboutUs/>}/>
 
-            <Route path="/ProductAndService" element={<ProductAndService/>} /> 
+                        <Route path="/ProductAndService" element={<ProductAndService/>}/>
 
-            <Route path="/HowToOrder" element={<HowToOrder/>} /> 
+                        <Route path="/HowToOrder" element={<HowToOrder/>}/>
 
-            <Route path="/FAQ" element={<FAQ/>} /> 
+                        <Route path="/FAQ" element={<FAQ/>}/>
 
-            <Route path="/FAQ_en" element={<FAQ_en/>} /> 
+                        <Route path="/FAQ_en" element={<FAQ_en/>}/>
 
-            <Route path="/Privacy" element={<Privacy/>} /> 
+                        <Route path="/Privacy" element={<Privacy/>}/>
 
-            <Route path="/ContactUs" element={<ContactUs/>} /> 
-          </Routes>
+                        <Route path="/ContactUs" element={<ContactUs/>}/>
+                    </Routes>
 
 
-       </Router>
-       </Provider>
-    </div>
-  );
+                </Router>
+            </Provider>
+        </div>
+    );
 }
 
 export default App;
